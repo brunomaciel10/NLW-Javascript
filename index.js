3ec9a0e92f14b3114870baa6f189a2833e5176d2
brunomaciel <brunomaciel0302@gmail.com>
2024-09-11 17:44:18 -0300

    criação do menu da aplicação

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-const start = () => {
-    
-};  
+// usando o INQUIRER => é um módulo node.js para PROMPTS interativos
+const {select} = require("@inquirer/prompts");
+
+const start = async () => {
+    while(true) {
+
+        // define as opções que irão aparecer nos prompts 
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        });
+
+        // irá executar uma ação de acordo com a opção escolhida
+        switch(opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar");
+            break;
+            case "listar":
+                console.log("vamos listar")
+            break;
+            case "sair":
+                console.log("Até a próxima!");
+                return
+        };
+    };
+};
 start();
